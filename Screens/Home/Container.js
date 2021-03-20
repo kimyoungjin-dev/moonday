36522b18591eb5e9874f10dabe14d43a30575345
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-20 22:34:27 +0900

    Get api information from container.

--- Screens/Home/Container.js
+++ Screens/Home/Container.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Presenter from "./Presenter";
+import { moonApi } from "../../api";
 
 const Container = () => {
+  const getData = async () => {
+    const data = await moonApi();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <Presenter />;
 };
 
