1fc7214ab2041e812302f41744acb027d152f5cb
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-22 19:57:02 +0900

    Create Loading.js

---
+++ Components/Loading.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.Text`
+  font-size: 30px;
+`;
+
+const Loading = () => {
+  return (
+    <Container>
+      <Text>...Loading</Text>
+    </Container>
+  );
+};
+
+export default Loading;
