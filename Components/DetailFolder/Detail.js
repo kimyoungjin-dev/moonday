bd4f77995a86b8bf610a0826700d847934701750
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-28 20:28:23 +0900

    Divide the detail screen up and down.

--- Components/DetailFolder/Detail.js
+++ Components/DetailFolder/Detail.js
@@ -1,83 +1,56 @@
+import moment from "moment";
 import React from "react";
-import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-import IconList from "./IconList";
+import BottomContainer from "./BottomContainer";
+import TopContainer from "./TopContainer";
 
 const Container = styled.View`
+  height: 100%;
   align-items: center;
+  padding-left: 30px;
+  padding-top: 100px;
   background-color: black;
-  padding-bottom: 40px;
-`;
-const MoonContainer = styled.View``;
-const DataContainer = styled.View`
-  width: 100%;
-`;
-const Contents = styled.View`
-  justify-content: space-around;
-  flex-direction: row;
-  width: 100%;
-  margin-bottom: 25px;
 `;
 
-const DetailContainer = ({ toggleEditing, data }) => {
+const Image = styled.Image`
+  height: 150%;
+  width: 150%;
+  position: absolute;
+  left: 100;
+  top: -160;
+`;
+
+const DetailContainer = ({ data }) => {
+  const mementData = moment();
+  const shortMonth = mementData._locale._monthsShort;
   //full moon
   const fullYear = new Date(data?.fm?.ut * 1000).getFullYear();
-  const fullMonth = new Date(data?.fm?.ut * 1000).getMonth() + 1;
+  const fullMonth = new Date(data?.fm?.ut * 1000).getMonth();
+  const fullMonthText = shortMonth[fullMonth];
   const fullDate = new Date(data?.fm?.ut * 1000).getDate();
 
   //next moon
   const newYear = new Date(data?.nnm?.ut * 1000).getFullYear();
   const newMonth = new Date(data?.nnm?.ut * 1000).getMonth() + 1;
+  const newMonthText = shortMonth[newMonth];
   const newDate = new Date(data?.nnm?.ut * 1000).getDate();
 
   return (
     <Container>
-      <TouchableOpacity onPress={() => toggleEditing()}>
-        <MoonContainer
-          style={{
-            shadowColor: "white",
-            shadowOpacity: 0.5,
-            shadowRadius: 10,
-            shadowOffset: {
-              width: 0,
-              height: 0,
-            },
-          }}
-        />
-      </TouchableOpacity>
-      <DataContainer>
-        <Contents>
-          <IconList
-            name={"circle-thin"}
-            value={`${newYear} · ${newMonth} · ${newDate}`}
-            text={"New Moon"}
-            fontAwesome={true}
-          />
-
-          <IconList
-            name={"sun-o"}
-            value={`${Math.round(data?.dfs)} km`}
-            text={"Distance sun"}
-            fontAwesome={true}
-          />
-        </Contents>
-
-        <Contents>
-          <IconList
-            text={"Full Moon"}
-            value={`${fullYear} · ${fullMonth} · ${fullDate}`}
-            name={"circle"}
-            fontAwesome={true}
-          />
-
-          <IconList
-            text={"Distance Earth"}
-            value={`${Math.round(data?.dfcoe)} km`}
-            name={"earth"}
-            fontAwesome={false}
-          />
-        </Contents>
-      </DataContainer>
+      <Image
+        resizeMode="contain"
+        source={require("../../assets/DetailBackground.jpg")}
+      />
+      <TopContainer />
+      <BottomContainer
+        fullYear={fullYear}
+        fullDate={fullDate}
+        fullMonthText={fullMonthText}
+        newYear={newYear}
+        newMonthText={newMonthText}
+        newDate={newDate}
+        data={data}
+      />
     </Container>
   );
 };
